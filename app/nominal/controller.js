86f569d3ee4c24a2c01239fd742d8c07214b89c3
Nailul Firdaus <nail.nafir@gmail.com>
2023-01-22 23:03:14 +0700

    add CRUD (update) in nominal page

--- app/nominal/controller.js
+++ app/nominal/controller.js
@@ -42,36 +42,36 @@ module.exports = {
             res.redirect('nominal');
         }
     },
-    // viewEdit: async (req, res) => {
-    //     try {
-    //         const { id } = req.params;
+    viewEdit: async (req, res) => {
+        try {
+            const { id } = req.params;
 
-    //         let category = await CategoryModel.findOne({ _id: id });
+            let nominal = await NominalModel.findOne({ _id: id });
 
-    //         res.render('admin/category/edit', { category });
-    //     } catch (error) {
-    //         req.flash('alertMessage', `${error.message}`);
-    //         req.flash('alertStatus', 'danger');
-    //         res.redirect('category');
-    //     }
-    // },
-    // actionEdit: async (req, res) => {
-    //     try {
-    //         const { id } = req.params;
-    //         const { name } = req.body;
+            res.render('admin/nominal/edit', { nominal });
+        } catch (error) {
+            req.flash('alertMessage', `${error.message}`);
+            req.flash('alertStatus', 'danger');
+            res.redirect('nominal');
+        }
+    },
+    actionEdit: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const { coinName, coinQuantity, price } = req.body;
 
-    //         await CategoryModel.findOneAndUpdate({ _id: id }, { name });
+            await NominalModel.findOneAndUpdate({ _id: id }, { coinName, coinQuantity, price });
 
-    //         req.flash('alertMessage', "Berhasil ubah data");
-    //         req.flash('alertStatus', "success");
+            req.flash('alertMessage', "Berhasil ubah data");
+            req.flash('alertStatus', "success");
 
-    //         res.redirect('/category');
-    //     } catch (error) {
-    //         req.flash('alertMessage', `${error.message}`);
-    //         req.flash('alertStatus', 'danger');
-    //         res.redirect('category');
-    //     }
-    // },
+            res.redirect('/nominal');
+        } catch (error) {
+            req.flash('alertMessage', `${error.message}`);
+            req.flash('alertStatus', 'danger');
+            res.redirect('nominal');
+        }
+    },
     // actionDelete: async (req, res) => {
     //     try {
     //         const { id } = req.params;
