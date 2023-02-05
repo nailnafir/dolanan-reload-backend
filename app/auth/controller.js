4fedd03bd37011b188ee18a8e28fee025cd21037
Nailul Firdaus <nail.nafir@gmail.com>
2023-02-05 16:38:02 +0700

    add API sign in

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -2,6 +2,8 @@ const PlayerModel = require('../player/model');
 const path = require('path');
 const fs = require('fs');
 const config = require('../../config');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
     signUp: async (req, res, next) => {
@@ -55,5 +57,35 @@ module.exports = {
             }
             next(error);
         }
+    },
+    signIn: (req, res, next) => {
+        const { email, password } = req.body;
+
+        PlayerModel.findOne({ email: email }).then((player) => {
+            if (player) {
+                const checkPassword = bcrypt.compareSync(password, player.password);
+                if (checkPassword) {
+                    const token = jwt.sign({
+                        player: {
+                            id: player.id,
+                            username: player.username,
+                            email: player.email,
+                            name: player.name,
+                            phoneNumber: player.phoneNumber,
+                            avatar: player.avatar,
+                        }
+                    }, config.jwtKey);
+
+                    res.status(200).json({ data: { token } });
+                } else {
+                    res.status(403).json({ message: 'Password yang Anda masukkan salah' });
+                }
+            } else {
+                res.status(403).json({ message: 'Email yang Anda masukkan belum terdaftar' });
+            }
+        }).catch((error) => {
+            res.status(500).json({ message: error.message || 'Terjadi Kesalahan Pada Server' });
+            next();
+        });
     }
 }
--- app/auth/router.js
+++ app/auth/router.js
@@ -3,8 +3,9 @@ var router = express.Router();
 var multer = require('multer');
 var os = require('os');
 
-const { signUp } = require('./controller');
+const { signUp, signIn } = require('./controller');
 
 router.post('/signUp', multer({ dest: os.tmpdir() }).single('image'), signUp);
+router.post('/signIn', signIn);
 
 module.exports = router;
