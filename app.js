28907749b9b888d8ab63c8cd070ccf7916d926b2
Nailul Firdaus <nail.nafir@gmail.com>
2023-02-05 20:07:01 +0700

    add cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var methodOverride = require('method-override');
 var session = require('express-session');
 var flash = require('connect-flash');
+var cors = require('cors');
 
 var dashboardRouter = require('./app/dashboard/router');
 var categoryRouter = require('./app/category/router');
@@ -20,6 +21,8 @@ var authRouter = require('./app/auth/router');
 
 var app = express();
 var URL = '/api/v1';
+
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
